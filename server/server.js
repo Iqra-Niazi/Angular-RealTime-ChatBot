c6da4863020e73c9db89bbade03769e8eebe9ae3
Iqra-Niazi <108399208+Iqra-Niazi@users.noreply.github.com>
2024-02-25 19:52:29 +0500

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+    cors: {
+        origins: [
+			"http://localhost:4200",
+			"http://localhost:8080",
+		],
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Heello world');
+})
+
+let userList = new Map();
+
+io.on('connection', (socket) => {
+    let userName = socket.handshake.query.userName;
+    addUser(userName, socket.id);
+
+    socket.broadcast.emit('user-list', [...userList.keys()]);
+    socket.emit('user-list', [...userList.keys()]);
+
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message-broadcast', {message: msg, userName: userName});
+    })
+
+    socket.on('disconnect', (reason) => {
+        removeUser(userName, socket.id);
+    })
+});
+
+function addUser(userName, id) {
+    if (!userList.has(userName)) {
+        userList.set(userName, new Set(id));
+    } else {
+        userList.get(userName).add(id);
+    }
+}
+
+function removeUser(userName, id) {
+    if (userList.has(userName)) {
+        let userIds = userList.get(userName);
+        if (userIds.size == 0) {
+            userList.delete(userName);
+        }
+    }
+}
+
+http.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running ${process.env.PORT || 3000}`);
+    console.log(`Connected Users` + ' : ' + userList);
+});
